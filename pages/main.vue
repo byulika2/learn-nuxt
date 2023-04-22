d912765a09a97916eb342d31bfa73880b5213698
MiracleHeo <byulika2@gmail.com>
2023-04-23 06:39:45 +0900

    init

---
+++ pages/main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>메인 페이지 입니다</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  async created() {
+    const response = await axios.get('http://localhost:3000/products')
+    console.log(response)
+  }
+}
+</script>
